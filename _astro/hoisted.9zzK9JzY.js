9e1407d505a8122a679ba5709bcfac45f6417171
grey6966 <grey6966@users.noreply.github.com>
2024-04-03 21:36:51 +0000

    deploy: 18270e5b3c602e53c603b860e60f833a215fcf1a

--- _astro/hoisted.9zzK9JzY.js
+++
@@ -1,1 +0,0 @@
-import"./hoisted.N2XxB48v.js";function t(){console.log("button clicked!")}document.getElementById("button").addEventListener("click",t);
---
+++ _astro/hoisted.CQEUBYu6.js
@@ -0,0 +1,1 @@
+import"./hoisted.N2XxB48v.js";function e(){console.log("showWechatPic")}document.getElementById("weChatBtn").addEventListener("click",e);
