5d0f833bb178720e5847129cc9ca86962a35ed35
grey <jeeun2023@outlook.com>
2024-04-12 10:40:40 +0800

    123

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://grey6966.github.io',
-  integrations: [mdx(), sitemap(), tailwind()],
-  server: { port: 8881 }
+    site: 'https://grey6966.github.io',
+    integrations: [mdx(), sitemap(), tailwind()],
+    /*  server: { port:  8080}*/
 });
